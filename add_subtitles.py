2f663737659110aa2fbf1e61988934531f0db325
Nick Pai <weichenpai57@gmail.com>
2026-08-20 10:24:52 +0800

    Reduce hardcoded subtitle font size for cleaner look

--- add_subtitles.py
+++ add_subtitles.py
@@ -37,7 +37,7 @@ COMPUTE_TYPE = "float16"
 LANGUAGE = "en"
 BEAM_SIZE = 5
 VAD_FILTER = True
-HARDCODE_STYLE = "FontSize=24,PrimaryColour=&HFFFFFF,OutlineColour=&H000000,Outline=3,Alignment=2,MarginV=60"
+HARDCODE_STYLE = "FontSize=14,PrimaryColour=&HFFFFFF,OutlineColour=&H40000000,Outline=1,Shadow=0.5,Alignment=2,MarginV=30"
 
 
 def extract_audio(video_path: Path, audio_path: Path) -> None:
